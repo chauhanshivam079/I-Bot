6621ea4d5bd0a0feb7d1fdf9b72c24c8cf1ef487
chauhanshivam079 <chauhanshivam079@gmail.com>
2022-07-20 14:04:16 +0530

    new functionality added

--- bot_modules/randomWord.js
+++ bot_modules/randomWord.js
@@ -1,10 +1,11 @@
-const { checkServerIdentity } = require("tls");
+const axios = require("axios");
+
 
 class RandomWord{
     static async getRandomWord(sock,chatId,msg){
         try{
-        const result=await fetch("https://random-words-api.vercel.app/word/");
-        const ans=`Word :- ${result[0].word}\n\n Meaning:- ${result[0].definition}`;
+        const result=await axios({url:"https://random-words-api.vercel.app/word/",mode:"cors",method:"GET"});
+        const ans=`Word :- ${result.data[0].word}\n\nMeaning:- ${result.data[0].definition}`;
         await sock.sendMessage(chatId,{text:ans},{quoted:msg});
         }
         catch(err){
