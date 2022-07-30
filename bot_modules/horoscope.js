36e946afbe26bf1ee6579ae8b72da62613f5ef72
chauhanshivam079 <chauhanshivam079@gmail.com>
2022-07-30 14:43:30 +0530

    minor changes

--- bot_modules/horoscope.js
+++ bot_modules/horoscope.js
@@ -3,15 +3,23 @@ const { Module } = require('module');
 
 class Horoscope{
     #url;
+    #horoArray;
     constructor(msgData){
-        this.#url=`https://aztro.sameerkumar.website/?sign=${msgData.msgData}&day=today`;
+        this.#url=`https://aztro.sameerkumar.website/?sign=${msgData.msgText}&day=today`;
+        this.#horoArray=["aries","taurus","gemini","cancer","Leo","Virgo","libra","scorpio","sagittarius","capricorn","aquarius","pisces"];
     }
         async getHoroscope(sock,chatId,msg){
         try{
-            let result=await axios.post(`${this._url}`);
-            result=result.data;
-            result=`*Date Range:-* ${result.date_range}\n*Today's Date:-* ${result.current_date}\n*Nature Hold's For You:-* ${result.description}\n*Compatibility:-* ${result.compatibility}\n*Mood:-* ${result.mood}\n*Color:-* ${result.color}\n*Lucky Number:-* ${result.lucky_number}\n*Lucky Time:-* ${result.lucky_time}`;
-            await sock.sendMessage(chatId,{text:result},{quoted:msg});
+            let index=this.#horoArray.indexOf(msgData.msgText);
+            if(index===-1){
+                await sock.sendMessage(chatId,{text:"Enter the right spelling"},{quoted:msg});
+            }
+            else{
+                let result=await axios.post(this.#url);
+                result=result.data;
+                result=`*Date Range:-* ${result.date_range}\n*Today's Date:-* ${result.current_date}\n*Nature Hold's For You:-* ${result.description}\n*Compatibility:-* ${result.compatibility}\n*Mood:-* ${result.mood}\n*Color:-* ${result.color}\n*Lucky Number:-* ${result.lucky_number}\n*Lucky Time:-* ${result.lucky_time}`;
+                await sock.sendMessage(chatId,{text:result},{quoted:msg});
+            }
         }  
         catch(err){
             await sock.sendMessage(chatId,{text:`${err.message}`},{quoted:msg});
