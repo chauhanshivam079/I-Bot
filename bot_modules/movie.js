90278ef3acf906a309554c2e31759b3925e33b1f
chauhanshivam079 <chauhanshivam079@gmail.com>
2022-08-15 22:00:36 +0530

    movie test 3

--- bot_modules/movie.js
+++ bot_modules/movie.js
@@ -1,45 +1,46 @@
-const axios=require("axios");
-const cheerio=require("cheerio");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-class MovieLinks{
-static async getLinks(sock,chatId,msg,msgData){
-    try{
-    let url=`https://pronoob-aio-drive.cf/Sct?search=${msgData.msgText.replaceAll(" ","+")}`;
-    console.log(url);
-    // Fetch HTML of the page we want to scrape
-    const {data}=await axios.get(url);
-    // Load HTML we fetched in the previous line
-    const $ = cheerio.load(data);
-    const list=$(".m-2 div[title] span a");
-    let prefix="https://pronoob-aio-drive.cf/";
-    let movieList="";
-    if(list.length===0){
-        await sock.sendMessage(chatId,{text:`Movie not found! Give correct name dumbo`},{quoted:msg});
-        return;
-    }
-    console.log("movie list",list.length);
-    list.each((idx,el)=>{
-        if(idx%2==0){
-            if(idx===list.length-2){
-                let temp=$(el).attr("href");
-                movieList=movieList+prefix+temp+"\n";
+class MovieLinks {
+    static async getLinks(sock, chatId, msg, msgData) {
+        try {
+            let url = `https://pronoob-aio-drive.cf/Sct?search=${msgData.msgText.replaceAll(
+        " ",
+        "+"
+      )}`;
+            console.log(url);
+            // Fetch HTML of the page we want to scrape
+            const { data } = await axios.get(url);
+            // Load HTML we fetched in the previous line
+            const $ = cheerio.load(data);
+            const list = $(".m-2 div[title] span a");
+            let prefix = "https://pronoob-aio-drive.cf/";
+            let movieList = `🍿👀📽\n`;
+
+            if (list.length === 0) {
+                await sock.sendMessage(
+                    chatId, { text: `Movie not found! Give correct name dumbo` }, { quoted: msg }
+                );
+                return;
             }
-            else if(!(idx&1))  {
-                if(idx<1){
-                let temp=$(el).attr("href");
-                movieList=movieList+prefix+temp+"\n";
+            console.log("Movie list", list.length);
+            list.each((idx, el) => {
+                if (idx % 2 == 0) {
+                    if (!(idx & 1)) {
+                        let temp = $(el).attr("href");
+                        movieList = movieList + prefix + temp + "\n" + "\n🍿👀📽\n\n";
+                    }
                 }
-            }   
+            });
+            let finalList = `${msgData.msgText} Links \n${movieList}`;
+            console.log(finalList);
+            await sock.sendMessage(chatId, { text: finalList }, { quoted: msg });
+        } catch (err) {
+            console.log(err);
+            await sock.sendMessage(
+                chatId, { text: `${err.message}` }, { quoted: msg }
+            );
         }
-    });
-    let finalList=`${movieList}`;
-    console.log(finalList);
-    await sock.sendMessage(chatId,{text:finalList},{quoted:msg});
+    }
 }
-catch(err){
-    console.log(err);
-    await sock.sendMessage(chatId,{text:`${err.message}`},{quoted:msg});
-}
-}
-}
-module.exports=MovieLinks;
+module.exports = MovieLinks;
