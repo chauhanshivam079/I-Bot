3c8f686014398c368d06d2c3083e5ed5759a5a47
chauhanshivam079 <chauhanshivam079@gmail.com>
2022-08-15 17:06:28 +0530

    movie downloader

---
+++ bot_modules/movie.js
@@ -0,0 +1,35 @@
+const axios=require("axios");
+const cheerio=require("cheerio");
+
+class MovieLinks{
+static async getLinks(sock,chatId,msg,msgData){
+    try{
+    let url=`https://pronoob-aio-drive.cf/Sct?search=${msgData.msgText.replaceAll(" ","+")}`;
+    // Fetch HTML of the page we want to scrape
+    const {data}=await axios.get(url);
+    // Load HTML we fetched in the previous line
+    const $ = cheerio.load(data);
+    const list=$(".m-2 div[title] span a");
+    let prefix="https://pronoob-aio-drive.cf/";
+    let movieList="";
+    if(list.length===0){
+        await sock.sendMessage(chatId,{text:`Movie not found! Give correct name dumbo`},{quoted:msg});
+        return;
+    }
+    list.each((idx,el)=>{
+        if(idx%2==0){
+            if(!(idx&1))  {
+                let temp=$(el).attr("href");
+                movieList="🍿👀📽"+"\n"+movieList+prefix+temp+"\n\n";
+            }   
+        }
+    });
+    await sock.sendMessage(chatId,{text:`*${msgData.msgText} Links*\n${movieList}`},{quoted:msg});
+}
+catch(err){
+    console.log(err);
+    await sock.sendMessage(chatId,{text:`${err.message}`},{quoted:msg});
+}
+}
+}
+module.exports=MovieLinks;
