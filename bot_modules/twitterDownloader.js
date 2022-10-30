0f6ede75a6c010d1678e13d4c93549217c73ce54
chauhanshivam079 <chauhanshivam079@gmail.com>
2022-10-30 18:03:18 +0530

    twitter downloader fixed

--- bot_modules/twitterDownloader.js
+++ bot_modules/twitterDownloader.js
@@ -1,38 +1,40 @@
-const { url } = require("inspector");
-const twitterPostDownloader=require("twitter-url-direct");
+const axios=require("axios");
 
 class TwitterDownloader{
     static async postDownload(sock,chatId,msg,msgData){
         try{
-        const response=await twitterPostDownloader(msgData.msgText);
-        if(response.found){
-            if(response.type==="image"){
-                await sock.sendMessage(chatId,{
-                    image:{url:response.download}
-                },
-                {quoted:msg});
+            let temp=msgData.msgText;
+            if(temp.length===0){
+                await sock.sendMessage(chatId,{text:`Enter url to be downloaded`},{quoted:msg});
+                return;
             }
-            else{
-                let link=response.download[response.download.length-1].url;
-                const check="https://ssstwitter.com";
-                if(link.includes(check)){
-                    link=link.split(check)[1];
+            temp=temp.split("/")[5].split("?")[0];
+            let baseUrl="https://tweetpik.com/api/tweets";
+            axios({url:`${baseUrl}/${temp}`}).then(async res=>{
+                const type=res.data.media[0].type;
+                if(type==="photo"){
+                    for(let i=0;i<res.data.media.length;i++){
+                        await sock.sendMessage(chatId,{
+                            image:{url:res.data.media[i].url}
+                        },
+                        {quoted:msg});
+                    }
                 }
-                await sock.sendMessage(chatId,{
-                                                video:{url:link},
-                                                caption:"",
-                                                gifPlayback: false,
-                                              },
-                                              {quoted:msg});
-            }
-        }
-            else{
-                await sock.sendMessage(chatId,{text:`Give correct link`},{quoted:msg});
-            }
+                if(type==="animated_gif" || type==="video"){
+                    axios({url:`${baseUrl}/${temp}/video`}).then(async res=>{
+                        await sock.sendMessage(chatId,{
+                            video:{url:res.data.variants[res.data.variants.length-1].url},
+                            caption:"",
+                            gifPlayback: false,
+                          },
+                          {quoted:msg});
+                    });
+                }
+            });
         }
         catch(err){
             console.log(err);
-            await sock.sendMessage(chatId,{text:`${err.message}`},{quoted:msg});
+            await sock.sendMessage(chatId,{text:err.message},{quoted:msg})
         }
     }
 }
