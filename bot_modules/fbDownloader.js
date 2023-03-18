425b1cff94f9c4dabefb4c1e59a7bf784472369c
chauhanshivam079 <chauhanshivam079@gmail.com>
2023-03-18 17:40:34 +0530

    favebook downloader added

---
+++ bot_modules/fbDownloader.js
@@ -0,0 +1,38 @@
+const axios=require("axios");
+require("dotenv").config({ path: "./Keys.env" })
+
+class FacebookDownloader{
+    static async postDownload(sock,chatId,msg,msgData){
+        try{
+            let temp=msgData.msgText;
+            if(temp.length===0){
+                await sock.sendMessage(chatId,{text:`Enter url to be downloaded`},{quoted:msg});
+                return;
+            }
+            const res = await axios.post(process.env.smd,{
+                url:temp
+            });
+                if(res.data.url[0].type === "jpg"){
+                    await sock.sendMessage(chatId,{
+                            image:{url:res.data.url[0].url}
+                        },
+                        {quoted:msg});
+                        return;
+                }
+                if(res.data.url[0].type === "mp4"){
+                    await sock.sendMessage(chatId,{
+                            video:{url:res.data.url[0].url},
+                            caption:`${res.data.url[0].subname}`,
+                            gifPlayback: false,
+                          },
+                          {quoted:msg});
+                          return;
+                }
+        }
+        catch(err){
+            console.log(err);
+            await sock.sendMessage(chatId,{text:err.message},{quoted:msg});
+        }
+    }
+}
+module.exports = FacebookDownloader;
