fb3627af15ec6a095a222103c4c400db74fb5ca5
Shivam Chauhan <72668899+chauhanshivam079@users.noreply.github.com>
2025-09-26 23:10:59 +0530

    Update aiImage.js

--- bot_modules/aiImage.js
+++ bot_modules/aiImage.js
@@ -1,54 +1,58 @@
-const {Configuration,OpenAIApi}=require("openai");
-require("dotenv").config({ path: "./Keys.env" });
-const { downloadContentFromMessage } = require("@adiwajshing/baileys");
-const config=new Configuration({
-    apiKey:process.env.openai,
-});
-const openAi=new OpenAIApi(config);
+// const {Configuration,OpenAIApi}=require("openai");
+// require("dotenv").config({ path: "./Keys.env" });
+// const { downloadContentFromMessage } = require("@adiwajshing/baileys");
+// const config=new Configuration({
+//     apiKey:process.env.openai,
+// });
+// const openAi=new OpenAIApi(config);
 class AiImageGeneration{
     static async aiImageGeneration(sock,chatId,msg,msgData){
         try{
-            let ques;
-        if (msgData.isQuoted && msgData.quotedMessage.quotedMessage.conversation) {
-            ques = msgData.quotedMessage.quotedMessage.conversation;
-        } else {
-            ques = msgData.msgText;
-        }
-        if(ques.length===0){
-            await sock.sendMessage(chatId,{text:`Are bhaiya kya search krna wo to likho`},{quoted:msg});
-        }
-            const response=await openAi.createImage({
-                prompt:ques,
-                n:1,
-            });
-            await sock.sendMessage(chatId,{image: { url: response.data.data[0].url }},{quoted:msg});
+                        await sock.sendMessage(chatId,{text:`command temporarily disabled`},{quoted:msg});
+return;
+        //     let ques;
+        // if (msgData.isQuoted && msgData.quotedMessage.quotedMessage.conversation) {
+        //     ques = msgData.quotedMessage.quotedMessage.conversation;
+        // } else {
+        //     ques = msgData.msgText;
+        // }
+        // if(ques.length===0){
+        //     await sock.sendMessage(chatId,{text:`Are bhaiya kya search krna wo to likho`},{quoted:msg});
+        // }
+        //     // const response=await openAi.createImage({
+        //     //     prompt:ques,
+        //     //     n:1,
+        //     // });
+        //     await sock.sendMessage(chatId,{image: { url: response.data.data[0].url }},{quoted:msg});
         }catch(err){
             await sock.sendMessage(chatId,{text:`${err.message}`},{quoted:msg});
         }
     }
     static async aiImageVariation(sock,chatId,msg,msgData){
         try{
-            let buffer;
-            let data;
-            if(msgData.isQuoted && msgData.quotedMessage.quotedMessage.hasOwnProperty("imageMessage")){
-                data=msgData.quotedMessage.quotedMessage;
-            }else{
-                await sock.sendMessage(chatId,{text:`Tag a image message to get the variation`},{quoted:msg});
-            }
-            if(data.hasOwnProperty("imageMessage")){
-                const stream = await downloadContentFromMessage(
-                    data.imageMessage,
-                    "image"
-                );
-                buffer = Buffer.from([]);
-                for await (const chunk of stream) {
-                    buffer = Buffer.concat([buffer, chunk]);
-                }
-                buffer.name="image.png";
-                const response =await openAi.createImageVariation(
-                    buffer,
-                    1);
-                await sock.sendMessage(chatId,{image:{url:response.data.data[0].url}},{quoted:msg});
+             await sock.sendMessage(chatId,{text:`command temporarily disabled`},{quoted:msg});
+return;
+            // let buffer;
+            // let data;
+            // if(msgData.isQuoted && msgData.quotedMessage.quotedMessage.hasOwnProperty("imageMessage")){
+            //     data=msgData.quotedMessage.quotedMessage;
+            // }else{
+            //     await sock.sendMessage(chatId,{text:`Tag a image message to get the variation`},{quoted:msg});
+            // }
+            // if(data.hasOwnProperty("imageMessage")){
+            //     const stream = await downloadContentFromMessage(
+            //         data.imageMessage,
+            //         "image"
+            //     );
+            //     buffer = Buffer.from([]);
+            //     for await (const chunk of stream) {
+            //         buffer = Buffer.concat([buffer, chunk]);
+            //     }
+            //     buffer.name="image.png";
+            //     const response =await openAi.createImageVariation(
+            //         buffer,
+            //         1);
+            //     await sock.sendMessage(chatId,{image:{url:response.data.data[0].url}},{quoted:msg});
             }
         }catch(err){
             await sock.sendMessage(chatId,{text:`${err.message}`},{quoted:msg});
